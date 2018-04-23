4df5e1cd7f4b01a3793638be78a2906ca2b73bc4
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-04-23 09:20:58 +0900

    Update transfer.js

--- examples/transfer.js
+++ examples/transfer.js
@@ -6,7 +6,7 @@ var privKey = process.env.privKey;
 let pKey = PrivateKey.fromWif(privKey);
 
 var MongoClient = require('mongodb').MongoClient;
-var url = MONGODB_URI;
+var url = process.env.MONGODB_URI;
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
