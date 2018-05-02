2fe16076f46f371088a2ee367382fc37645c4312
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-05-02 15:16:34 +0900

    Update transfer.js

--- examples/transfer.js
+++ examples/transfer.js
@@ -219,6 +219,19 @@ function handler(req, res){
     res.setHeader('Content-Type', 'text/html');
     res.writeHead(200);
 	//console.log('test',tarot.threeCardReading());
+	MongoClient.connect(url, function(err, db) {
+  		if (err) throw err;
+  		var dbo = db.db("heroku_9cf4z9w3");
+  		var query = { ispaid : "no" };
+  		dbo.collection("customers").aggregate(
+			[{$group : {_id : "$refer", num_tutorial : {$sum : 1}}}],
+  			function( err, data ) {
+    				if ( err ) throw err;
+    				console.log( JSON.stringify( data, undefined, 2 ) );
+  			}
+		);
+	});
+			
     //res.end("<html><body><h1>Hello</h1></body></html>");
 	var r1 = "<html><body><h1>";
 	var r2 = "</h1></body></html>";
