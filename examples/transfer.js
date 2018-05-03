e096820ad932b223f79b62178d9639acc029fdfe
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-05-03 14:13:30 +0900

    Update transfer.js

--- examples/transfer.js
+++ examples/transfer.js
@@ -318,7 +318,7 @@ app.set('views',"examples/views");
 app.get('/', function(req, res) {
 	getRank(function(drinks){
 		console.log("in appget",drinks);
-		var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+		var tagline = "TBD";
     		res.render('pages/index', {
         	drinks: drinks,
         	tagline: tagline
