726306d31ffa91b8f5dd87f7d69a5ecc1bb0b15d
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-05-03 14:11:09 +0900

    Update transfer.js

--- examples/transfer.js
+++ examples/transfer.js
@@ -225,8 +225,10 @@ function getRank(cb){
 	});
 	
 	var sumCars = (db, callback) => {
-		var agr = [{$group: {_id: "$refer", all: { $sum: 1 } }}, {$sort: {all: -1}}, 
-			   {$match: { refer: {$exists:true, $ne: null}}}]; 
+		var agr = [{$match: { refer: {$exists:true, $ne: null}}},
+			   {$group: {_id: "$refer", all: { $sum: 1 } }}, 
+			   {$sort: {all: -1}}
+			   ]; 
 		var dbo = db.db("heroku_9cf4z9w3");
 		var cursor = dbo.collection('customers').aggregate(agr).toArray( (err, res) => {
 			console.log(res);
